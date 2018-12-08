85f7a08f33927c8e9f724e67310cf3a3a19f41b7
Nguyen Tuan Hai Dang <31280959+hsgser@users.noreply.github.com>
2018-12-08 23:38:14 +0900

    Update Problem1_ProjectEuler.py

--- ProjectEuler/Problem1_ProjectEuler.py
+++ ProjectEuler/Problem1_ProjectEuler.py
@@ -1,5 +1,21 @@
-desired_numbers = [x for x in range(1,1000) if x%3 ==0 or x%5 == 0]
-total = 0
-for number in desired_numbers:
-    total += number
-print (total)
+"""
+https://projecteuler.net/problem=1
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.
+"""
+def multiple_3_or_5(n):
+    """
+    A utility function to calculate the sum of all multiples of 3 or 5 below n
+    Parameter:
+    n: a positive integer 
+    """
+    total = 0
+    desired_numbers = [x for x in range(1,n) if x%3 == 0 or x%5 == 0]
+    
+    for num in desired_numbers:
+        total += num
+    return total
+
+if __name__ == "__main__":
+    n = 1000
+    print("sum of all the multiples of 3 or 5 below {} is {}".format(n, multiple_3_or_5(n)))
